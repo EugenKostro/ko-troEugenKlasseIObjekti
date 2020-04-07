6b3579f11b1c4db2ceb9a1c17dbebd38fe9337b8
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-04-07 12:05:27 +0200

    Add files via upload

---
+++ koštroEugenKlaseIObjekti/koštroEugenKlaseIObjekti/GeometrijskoTijelo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koštroEugenKlaseIObjekti
+{
+    class GeometrijskoTijelo
+    {
+        double oplosje;
+        double volumen;
+        static int brojacTijela;
+
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+        public double Volumen { get => volumen; set => volumen = value; }
+        public static int BrojacTijela { get => brojacTijela; set => brojacTijela = value; }
+
+        public GeometrijskoTijelo()
+        {
+            brojacTijela++;
+        }
+    }
+}
